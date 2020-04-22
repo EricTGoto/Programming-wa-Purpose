deac1eb689f0f6a882079c53c2c38f699f129ce5
Eric <erictomokigoto@gmail.com>
2020-04-21 21:47:37 -0700

    removed unused variables, removed the for loop that existed on a false mathematical assumption

--- src/Ramanujan.java
+++ src/Ramanujan.java
@@ -4,24 +4,28 @@ public class Ramanujan {
 
     public static boolean isRamanujan(long n) {
         int biggestCube = (int) Math.cbrt(n);
-        int smallCube = 0;
         int lowerBound = 0;
-
-        for (int i = 1; i < biggestCube; i++) {
-            if (Math.pow(i, 3) + Math.pow(biggestCube, 3) == n) {
-                smallCube = i;
-            }
-        }
-        if (smallCube == 0) return false;
-
-        lowerBound = (int) Math.ceil(Math.cbrt(Math.pow(biggestCube, 3) - Math.pow(biggestCube - 1, 3)));
+        double checkNumRounded=0;
 
         for (int k = biggestCube - 1; k > lowerBound; k--) {
 
             double checkNum = Math.cbrt(n - Math.pow(k, 3));
-            double checkNumRounded = Math.rint(checkNum);
+            checkNumRounded = Math.rint(checkNum);
 
-            if ((checkNum - checkNumRounded) == 0) return true;
+            if ((checkNum - checkNumRounded) == 0) {
+                biggestCube=--k;
+                break;
+            }
+        }
+
+        for (int k=biggestCube;k>lowerBound;k--){
+
+            double checkNum=Math.cbrt(n-Math.pow(k,3));
+            double checkNumRounded2=Math.rint(checkNum);
+            if (checkNumRounded2==checkNumRounded) continue;
+
+            if (checkNum-checkNumRounded2==0) return true;
+
         }
 
         return false;
