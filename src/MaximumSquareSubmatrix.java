02bc2ec44060caad64704fc601358b25ba170772
Eric <erictomokigoto@gmail.com>
2020-04-22 09:40:49 -0700

    started working on the maximum square submatrix problem, question 3 of assn 7

---
+++ src/MaximumSquareSubmatrix.java
@@ -0,0 +1,14 @@
+public class MaximumSquareSubmatrix {
+    // Given an n-n matrix of 0s and 1s, find a contiguous square submatrix of maximum size that contains only 1s
+    // contiguous as in row and column indices must be continuous
+    // assume that the array is a square matrix only containing 0s and 1s
+
+    public static int size(int[][] a){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
