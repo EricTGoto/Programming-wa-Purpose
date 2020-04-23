d7d2fc3673cf8b84209e78fa5d6a3f78af506d44
Eric <erictomokigoto@gmail.com>
2020-04-23 15:15:54 -0700

    intial commit of assignment 8 problem 1

---
+++ src/Huntingtons.java
@@ -0,0 +1,23 @@
+public class Huntingtons {
+
+    // Returns the maximum number of consecutive repeats of CAG in the DNA string
+    public static int maxRepeats(String dna){
+
+    }
+
+    // Returns a copy of s, with all whitespace (spaces, tabs, and newlines) removed
+    public static String removeWhitespace(String s){
+
+
+    }
+
+    // Returns one of the diagnoses corresponding to the maximum number of repeats:
+    // "not human", "normal", "high risk", "Huntingtons"
+    public static String diagnose (int maxRepeats){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
